7e5e9751d712cd7fed84a85bfe6575a0aceb1a28
jyntn23 <jayantonroblico23@gmail.com>
2020-09-24 18:19:29 +0800

    contact and responsiveness

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const slideContainer = document.querySelector('.slide-container');
-const slide = document.querySelector('.slides');
-const nextBtn = document.getElementById('next-btn');
-const prevBtn = document.getElementById('prev-btn');
-const interval = 1000;
+const slideContainer = document.querySelector(".slide-container");
+const slide = document.querySelector(".slides");
+const nextBtn = document.getElementById("next-btn");
+const prevBtn = document.getElementById("prev-btn");
+const interval = 2000;
 
-let slides = document.querySelectorAll('.slide');
+let slides = document.querySelectorAll(".slide");
 let index = 1;
 let slideId;
 
 const firstClone = slides[0].cloneNode(true);
 const lastClone = slides[slides.length - 1].cloneNode(true);
 
-firstClone.id = 'first-clone';
-lastClone.id = 'last-clone';
+firstClone.id = "first-clone";
+lastClone.id = "last-clone";
 
 slide.append(firstClone);
 slide.prepend(lastClone);
@@ -22,49 +22,76 @@ const slideWidth = slides[index].clientWidth;
 slide.style.transform = `translateX(${-slideWidth * index}px)`;
 
 const startSlide = () => {
-    slideId = setInterval(() => {
-        moveToNextSlide();
-    }, interval)
-}
+  slideId = setInterval(() => {
+    moveToNextSlide();
+  }, interval);
+};
 
-const getSlides = () => slides = document.querySelectorAll('.slide');
+const getSlides = () => (slides = document.querySelectorAll(".slide"));
 
-slide.addEventListener('transitionend', () => {
-    slides = getSlides();
-    if(slides[index].id === firstClone.id){
-        slide.style.transition = 'none';
-        index = 1;
-        slide.style.transform = `translateX(${-slideWidth * index}px)`;
-    }
-    if(slides[index].id === lastClone.id){
-        slide.style.transition = 'none';
-        index = slides.length - 2;
-        slide.style.transform = `translateX(${-slideWidth * index}px)`;
-    }
-})
+slide.addEventListener("transitionend", () => {
+  slides = getSlides();
+  if (slides[index].id === firstClone.id) {
+    slide.style.transition = "none";
+    index = 1;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  }
+  if (slides[index].id === lastClone.id) {
+    slide.style.transition = "none";
+    index = slides.length - 2;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  }
+});
 
 const moveToNextSlide = () => {
-    slides = getSlides();
-    if(index >= slides.length -1) return;
-    index++;
-    slide.style.transform = `translateX(${-slideWidth * index}px)`;
-    slide.style.transition = '.7s';
-}
+  slides = getSlides();
+  if (index >= slides.length - 1) return;
+  index++;
+  slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  slide.style.transition = ".7s";
+};
 
 const moveToPrevSlide = () => {
-    if(index <= 0) return;
-    index--;
-    slide.style.transform = `translateX(${-slideWidth * index}px)`;
-    slide.style.transition = '.7s';
-}
+  if (index <= 0) return;
+  index--;
+  slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  slide.style.transition = ".7s";
+};
 
-slideContainer.addEventListener('mouseenter', () => {
-    clearInterval(slideId);
-})
+slideContainer.addEventListener("mouseenter", () => {
+  clearInterval(slideId);
+});
 
-slideContainer.addEventListener('mouseleave', startSlide);
+slideContainer.addEventListener("mouseleave", startSlide);
 
-nextBtn.addEventListener('click', moveToNextSlide);
-prevBtn.addEventListener('click', moveToPrevSlide);
+nextBtn.addEventListener("click", moveToNextSlide);
+prevBtn.addEventListener("click", moveToPrevSlide);
 
 startSlide();
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+
+    //Burger Animation
+    burger.classList.toggle("toggleBurger");
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.8
+        }s`;
+      }
+    });
+  });
+};
+
+navSlide();
